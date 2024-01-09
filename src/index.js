9569b84e6467d8325531bab675f8d3164ad1870a
emiride <emir.hodzich@gmail.com>
2024-01-09 06:30:46 +0100

    Updated node

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 import { downloadLatestRelease, unpackLatestRelease, appendToGitHubPath } from './functions/cardano-bins.js';
+
 dotenv.config();
 await downloadLatestRelease();
 await unpackLatestRelease();
 await appendToGitHubPath('./bins');
-//# sourceMappingURL=index.js.map
